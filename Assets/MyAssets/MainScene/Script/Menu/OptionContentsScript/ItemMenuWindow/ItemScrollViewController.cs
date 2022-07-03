e3fe00a564e6be3201407e00bdafa1bc1b93ec82
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-07-03 13:10:08 +0900

    アイテムウィンドウをキーボード操作できるよう改修

--- Assets/MyAssets/MainScene/Script/Menu/OptionContentsScript/ItemMenuWindow/ItemScrollViewController.cs
+++ Assets/MyAssets/MainScene/Script/Menu/OptionContentsScript/ItemMenuWindow/ItemScrollViewController.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ItemScrollViewController : MonoBehaviour
 {
+    [SerializeField] float _lineHeight;
+
+    bool _firstAdjustment = false;//最初は調整しない
+
+    GameObject _currentButton;
+    GameObject _beforeButton;
+
     void Start()
     {
 
@@ -11,40 +19,43 @@ public class ItemScrollViewController : MonoBehaviour
 
     void Update()
     {
-        //必要であれば、スクロールする。
-        if (!SelectedObjectInRectTop())
+        //なぜか最初の一週目はでたらめな数字が入っているので調整しない
+        if (!_firstAdjustment)
         {
-            ScrollViewTop();
+            _firstAdjustment = true;
         }
-        if (!SelectedObjectInRectBottom())
+        else
         {
-            ScrollViewBottom();
+            _currentButton = EventSystem.current.currentSelectedGameObject;
+            //必要であれば、スクロールする。
+            if (_currentButton != _beforeButton)
+            {
+                Update_ScrollPos();
+            }
+            _beforeButton = _currentButton;
         }
     }
 
-    /// <summary> 選択中のButtonが枠より上に、はみ出していないか判定する </summary>
-    /// <returns> 範囲内であればtrueを返す </returns>
-    bool SelectedObjectInRectTop()
-    {
-        //範囲内になければfalseを返す
-        return false;
-    }
 
-    /// <summary> 選択中のButtonが枠より下に、はみ出していないか判定する </summary>
-    bool SelectedObjectInRectBottom()
+    void Update_ScrollPos()
     {
-        //範囲内になければfalseを返す
-        return false;
-    }
+        if (_currentButton != null)
+        {
+            //上にはみ出していないか判定する
+            if (GetComponent<RectTransform>().anchoredPosition.y >//枠の上辺
+                -_currentButton.GetComponent<RectTransform>().anchoredPosition.y)//ボタンの上辺(枠が正の値に大きくなるのに対して、ボタンは負の値が大きくなるので、片方だけ正負反転する)
+            {
+                //はみ出していれば位置を調整する
+                GetComponent<RectTransform>().anchoredPosition = Vector2.up * -_currentButton.GetComponent<RectTransform>().anchoredPosition;
+            }
 
-    /// <summary> SelectedObjectInRect()がfalseを返したらスクロールする </summary>
-    void ScrollViewTop()
-    {
-        //Contentの位置を更新する
-    }
-
-    void ScrollViewBottom()
-    {
-
+            //下にはみ出していないか判定する
+            if (GetComponent<RectTransform>().anchoredPosition.y + 500 <//枠の下辺(500はScrollViewのHeight。シリアライズで取ってくるよう変更する)
+                -(_currentButton.GetComponent<RectTransform>().anchoredPosition.y - _lineHeight))//ボタンの下辺
+            {
+                //はみ出していれば位置を調整する
+                GetComponent<RectTransform>().anchoredPosition = Vector3.up * -(_currentButton.GetComponent<RectTransform>().anchoredPosition.y + 500f - _lineHeight);
+            }
+        }
     }
 }
