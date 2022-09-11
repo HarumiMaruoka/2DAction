af5d5e9d83da898e1262e37a9fd4afd7597dae3f
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-09-12 07:39:34 +0900

    [fix] 軽微な変更 : 変数名とコメントを修正

--- Assets/MyAssets/MainScene/Script/MovingScaffolding/MovingScaffoldinHorizontal.cs
+++ Assets/MyAssets/MainScene/Script/MovingScaffolding/MovingScaffoldinHorizontal.cs
@@ -8,14 +8,14 @@ using DG.Tweening;
 /// </summary>
 public class MovingScaffoldinHorizontal : MonoBehaviour
 {
-    [Header("横の移動量"), SerializeField] float _endPointX;
+    [Header("横の移動量"), SerializeField] float _AmountOfMovementX;
     [Header("掛ける時間"), SerializeField] float _moveTimeX;
 
 
     void Start()
     {
         transform.
-        DOMoveX(_endPointX, _moveTimeX). // X方向に移動する。_endPointX に向けて _moveTimeX 秒間掛けて。
+        DOMoveX(_AmountOfMovementX, _moveTimeX). // X方向に移動する。_AmountOfMovementX に向かって _moveTimeX 秒間掛けて。
         SetEase(Ease.Linear).            // 移動のニュアンスを付ける。 一覧 → https://game-ui.net/?p=835 
         SetLoops(-1, LoopType.Yoyo);     // 何回繰り返すか。-1で無限回。LoopTypeは繰り返し方法。
     }
